c7d86735794c603b0ca265bfe4f95ae529712832
Sh0tlan <x359623@ukr.net>
2025-02-06 15:46:12 +0200

    feat: add account layout

--- src/features/ui/AppButton.tsx
+++ src/features/ui/AppButton.tsx
@@ -1,35 +1,51 @@
-import { Button, type SxProps, type Theme, Typography } from '@mui/material';
+import LoadingButton from '@mui/lab/LoadingButton';
+import { type SxProps, type Theme, Typography } from '@mui/material';
 
 interface Props {
-  type: 'button' | 'submit' | 'reset';
+  type?: 'button' | 'submit' | 'reset';
   variant?: 'text' | 'contained' | 'outlined';
   fullWidth?: boolean;
+  loading?: boolean;
   children: React.ReactNode;
+  endIcon?: React.ReactNode;
+  onClick?: () => void;
+  LinkComponent?: React.ElementType;
+  href?: string;
   sx?: SxProps<Theme>;
 }
 
 export default function AppButton({
-  type,
+  type = 'button',
   variant = 'contained',
   fullWidth,
+  onClick,
   children,
+  href,
+  LinkComponent,
+  loading,
+  endIcon,
   sx,
 }: Props) {
   return (
-    <Button
+    <LoadingButton
+      LinkComponent={LinkComponent}
+      href={href}
+      loading={loading}
       fullWidth={fullWidth}
       type={type}
       variant={variant}
+      endIcon={endIcon}
       sx={{
         borderRadius: 2,
         height: { xs: 48, md: 56 },
         textTransform: 'none',
         ...sx,
       }}
+      onClick={onClick}
     >
       <Typography component="span" variant="body2">
         {children}
       </Typography>
-    </Button>
+    </LoadingButton>
   );
 }
