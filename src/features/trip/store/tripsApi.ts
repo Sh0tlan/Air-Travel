af8a1c2c97b6b239983fb04c274f9a60f48f3465
Sh0tlan <x359623@ukr.net>
2025-08-22 23:15:11 +0300

    feat: add trips fetching with rtk query

---
+++ src/features/trip/store/tripsApi.ts
@@ -0,0 +1,30 @@
+import { createApi, fakeBaseQuery } from '@reduxjs/toolkit/query/react';
+
+import { getTripById, getTrips } from '@services/api';
+
+import { Trip } from '../types';
+
+export const tripsApi = createApi({
+  reducerPath: 'tripsApi',
+  baseQuery: fakeBaseQuery(),
+  endpoints: (builder) => ({
+    getTrips: builder.query<Trip[], void>({
+      queryFn: async () => {
+        const data = await getTrips();
+        return {
+          data,
+        };
+      },
+    }),
+    getTripDetails: builder.query<Trip, string | undefined>({
+      queryFn: async (tripId) => {
+        const data = await getTripById(tripId);
+        return {
+          data,
+        };
+      },
+    }),
+  }),
+});
+
+export const { useGetTripsQuery, useGetTripDetailsQuery } = tripsApi;
