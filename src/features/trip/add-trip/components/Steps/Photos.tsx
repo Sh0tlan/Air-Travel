247d633a03ea54085f08e001cfe34e520641c452
Sh0tlan <x359623@ukr.net>
2026-01-09 00:32:53 +0000

    feat: add wizzard submission reset and redirect

--- src/features/trip/add-trip/components/Steps/Photos.tsx
+++ src/features/trip/add-trip/components/Steps/Photos.tsx
@@ -1,19 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+
+import { AppRoutes } from '@config/routes';
 import FilesForm from '@features/trip/components/Files/FilesForm';
+import { useAddTripMutation } from '@features/trip/store/tripsApi';
 import { TripFile } from '@features/trip/types';
-import { addTrip } from '@services/api/trip';
 import { useAppDispatch, useAppSelector } from '@store/index';
 
-import { selectWizardTrip, setPhotos } from '../../store/tripWizardSlice';
+import {
+  resetWizard,
+  selectWizardTrip,
+  setPhotos,
+} from '../../store/tripWizardSlice';
 import Pagination from '../Navigation/Pagination';
 
 export default function Photos() {
-  const { photos, onSubmit, onChange } = usePhotosForm();
+  const { photos, onSubmit, onChange, isLoading } = usePhotosForm();
 
   return (
     <FilesForm
       defaultFiles={photos}
       onSubmit={onSubmit}
-      SubmitComponent={<Pagination />}
+      SubmitComponent={<Pagination isLoading={isLoading} />}
       onChange={onChange}
       type={'photo'}
     />
@@ -21,16 +28,30 @@ export default function Photos() {
 }
 
 function usePhotosForm() {
+  const [addTrip, { isLoading }] = useAddTripMutation();
+  const navigate = useNavigate();
   const dispatch = useAppDispatch();
   const trip = useAppSelector(selectWizardTrip);
 
   const onSubmit = async (data: TripFile[]) => {
+    if (isLoading) {
+      return;
+    }
+
     dispatch(setPhotos(data));
 
-    await addTrip({
+    const result = await addTrip({
       ...trip,
       photos: data,
     });
+
+    if (!('error' in result)) {
+      navigate(AppRoutes.trips);
+      dispatch(resetWizard());
+    }
+
+    navigate(AppRoutes.trips);
+    dispatch(resetWizard());
   };
 
   const onChange = (data: TripFile[]) => {
@@ -41,5 +62,6 @@ function usePhotosForm() {
     photos: trip.photos,
     onSubmit,
     onChange,
+    isLoading,
   };
 }
--- src/features/trip/add-trip/store/tripWizardSlice.ts
+++ src/features/trip/add-trip/store/tripWizardSlice.ts
@@ -81,6 +81,11 @@ export const tripWizardSlice = createSlice({
     setPhotos: (state, action: PayloadAction<Trip['photos']>) => {
       state.trip.photos = action.payload;
     },
+    resetWizard: (state) => {
+      const initState = getInitialState();
+      state.currentStep = 0;
+      state.trip = initState.trip;
+    },
   },
   extraReducers: (buider) => {
     buider.addCase(PURGE, () => {
@@ -100,6 +105,7 @@ export const {
   setDocuments,
   setPhotos,
   setPackingLists,
+  resetWizard,
 } = tripWizardSlice.actions;
 
 export const selectCurrentStep = (state: RootState) =>
