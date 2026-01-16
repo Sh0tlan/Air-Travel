f45c38040e0c5c0d7b32aa53c2dff55aeaafce18
Sh0tlan <x359623@ukr.net>
2026-01-16 21:14:50 +0000

    feat: add packing list trip editing tab with autoupdate

---
+++ src/features/trip/edit/Tabs/PackingList.tsx
@@ -0,0 +1,25 @@
+import PackingForm from '@features/trip/components/PackingForm';
+import { type Trip } from '@features/trip/types';
+
+import ContentCard from './ContentTab';
+
+interface Props {
+  trip: Trip;
+  onUpdate: (data: Partial<Trip>) => void;
+}
+
+export default function PackingList({ trip, onUpdate }: Props) {
+  const onChange = (packingLists: Trip['packingLists']) => {
+    console.log('change');
+    onUpdate({ packingLists });
+  };
+
+  return (
+    <ContentCard title="Packing List">
+      <PackingForm
+        defaultPackingLists={trip.packingLists}
+        onChange={onChange}
+      />
+    </ContentCard>
+  );
+}
