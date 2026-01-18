b1697fa6adc64dfda37fb3d80bd3d74b2049f08e
Sh0tlan <x359623@ukr.net>
2026-01-18 22:17:06 +0000

    feat: add trip delete functionality

--- src/features/trip/edit/TripDetails.tsx
+++ src/features/trip/edit/TripDetails.tsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import DeleteIcon from '@mui/icons-material/Delete';
 import {
@@ -12,6 +12,10 @@ import {
 import { AppRoutes } from '@config/routes';
 import { Colors } from '@config/styles';
 import AppButton from '@features/ui/AppButton';
+import AppDialog from '@features/ui/AppDialog';
+import useDialog from '@hooks/useDialog';
+import useToast from '@hooks/useToast';
+import { deleteTrip } from '@services/api';
 
 import { useGetTripQuery, useUpdateTripMutation } from '../store/tripsApi';
 import { Trip } from '../types';
@@ -19,12 +23,22 @@ import Hero from './Hero/Hero';
 import TripTabs from './Tabs/TripTabs';
 
 export default function TripDetails() {
+  const navigate = useNavigate();
+  const { showSuccessMessage } = useToast();
+  const { isOpen, open, close } = useDialog();
   const { tripId } = useParams();
   const { data: trip, isLoading, isSuccess, isError } = useGetTripQuery(tripId);
   const [updateTrip] = useUpdateTripMutation();
 
   const onTripUpdate = (data: Partial<Trip>) => {
     updateTrip({ id: trip!.id, data });
+  };
+
+  const onDeleteTrip = async () => {
+    close();
+    navigate(AppRoutes.trips);
+    await deleteTrip(tripId!);
+    showSuccessMessage('Trip was successfully deleted!');
   };
 
   if (isLoading) {
@@ -51,12 +65,29 @@ export default function TripDetails() {
               {trip.name}
             </Typography>
           </Breadcrumbs>
-          <AppButton endIcon={<DeleteIcon />} isSmall color="error">
+          <AppButton
+            endIcon={<DeleteIcon />}
+            isSmall
+            color="error"
+            onClick={open}
+          >
             Delete
           </AppButton>
         </Stack>
         <Hero trip={trip} onUpdate={onTripUpdate} />
         <TripTabs trip={trip} onUpdate={onTripUpdate} />
+        <AppDialog
+          title="Are you sure that you want to delete this document?"
+          primaryButtonText="Yes"
+          secondaryButtonText="No"
+          isOpen={isOpen}
+          onClose={close}
+          onSecondaryButtonClick={close}
+          onPrimaryButtonClick={onDeleteTrip}
+          maxWidth={460}
+          disableBottomTitlePadding
+          hideCloseButton
+        />
       </Stack>
     );
   } else if (isError) {
